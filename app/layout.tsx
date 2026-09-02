567774785e0010ea7a2706fc897c9d204b5d647d
Ericke Castro <eerickevh@gmail.com>
2026-09-02 18:26:06 -0400

    Atualiza ícone e metadados do portfólio

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,44 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ericke Castro | Fullstack .NET Software Developer",
-  description: "Portfólio de Ericke Castro, desenvolvedor Fullstack .NET especializado em aplicações Web, APIs REST e soluções multiplataforma.",
+  metadataBase: new URL("https://ericke-castro.pages.dev"),
+
+  title: "Ericke Castro | Desenvolvedor .NET",
+
+  description:
+    "Portfólio de Ericke Castro, desenvolvedor Fullstack .NET especializado em aplicações Web, APIs REST e soluções multiplataforma.",
+
   icons: {
-    icon: "/favicon.svg",
-    shortcut: "/favicon.svg",
+    icon: "/favicon.png",
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+
+  openGraph: {
+    title: "Ericke Castro | Desenvolvedor .NET",
+    description:
+      "Projetos, experiências e competências no ecossistema .NET.",
+    url: "/",
+    siteName: "Portfólio | Ericke Castro",
+    locale: "pt_BR",
+    type: "website",
+
+    images: [
+      {
+        url: "/portfolio-preview.png",
+        width: 1200,
+        height: 630,
+        alt: "Portfólio profissional de Ericke Castro",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Ericke Castro | Desenvolvedor .NET",
+    description:
+      "Projetos, experiências e competências no ecossistema .NET.",
+    images: ["/portfolio-preview.png"],
   },
 };
 
